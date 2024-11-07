6545dc593fc514b9b5b4cdad8fe0a322768a4fb9
Suchet <57799038+bass-boop@users.noreply.github.com>
2024-11-07 05:41:28 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,33 @@
-let score = "20"
+let score = "mr mengu"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// <------------ NOTES ------------->
+// "20" => 20
+// "20abc" => NaN (Not A Number)
+// true => 1; false => 0
+
+// true and false is boolean
+
+let isLoggedIn = "sus"
+
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "penguin" => true
+
+
+let someNumber = 20
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
