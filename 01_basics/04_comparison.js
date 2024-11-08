e6f69cb09948769e202a89458520f26f0e599688
Suchet <57799038+bass-boop@users.noreply.github.com>
2024-11-08 08:34:14 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// datatypes different or changed
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// avoid below conversion/comparison clean code is preferred
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// ===
+
+console.log("2" == 2);
+console.log("2" === 2);
+
